c36e28df5af35df573a993f12f40142242efb2e5
Jinge <jinge_windows@hotmail.com>
2025-11-20 04:16:04 +1100

    启用api关机和睡眠功能

--- WPF_HomeTool/Services/WebServerStartup.cs
+++ WPF_HomeTool/Services/WebServerStartup.cs
@@ -44,7 +44,47 @@ namespace WPF_HomeTool.Services
         [HttpGet("{command}")]  //url有参情况下
         public string Get(string command)
         {
-            return command;
+            if (command == "shutdown")
+            {
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                //重定向输入、输出、异常信息
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.RedirectStandardError = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+                cmd.StandardInput.WriteLine("shutdown /s /t 0");
+                cmd.StandardInput.Flush();
+                cmd.StandardInput.Close();
+                string result = cmd.StandardOutput.ReadToEnd();
+                return result;
+            }
+            else if (command == "sleep")
+            {
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                //重定向输入、输出、异常信息
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.RedirectStandardError = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+                //需要在电源选项中关闭“混合睡眠”才能使该命令生效，否则会变成休眠，可以通过在windows系统中执行命令“powercfg -h off”关闭休眠功能
+                cmd.StandardInput.WriteLine("rundll32.exe powrprof.dll,SetSuspendState 0,1,0");
+                cmd.StandardInput.Flush();
+                cmd.StandardInput.Close();
+                string result = cmd.StandardOutput.ReadToEnd();
+                return result;
+            }
+            else
+            {
+                return command + " 命令没有对应指令";
+            }
+
+            //return command;
         }
     }
 }
